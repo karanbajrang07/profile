1daf89a4a6747e50d1be29f9367717f326ac1fa1
karanbk-eGov <karan.bajrang@egovernments.org>
2020-04-04 05:11:06 +0530

    Updates

---
+++ precache-manifest.559ac4a4d52e7e80cd474ce73045bca8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125c3ee893cdaac821ed42efa9dff5de",
+    "url": "/reactWebsite/index.html"
+  },
+  {
+    "revision": "de179da718e0592b9f65",
+    "url": "/reactWebsite/static/css/main.7870f376.chunk.css"
+  },
+  {
+    "revision": "d73ea675c45b43a980ec",
+    "url": "/reactWebsite/static/js/2.5c20e2d1.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/reactWebsite/static/js/2.5c20e2d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de179da718e0592b9f65",
+    "url": "/reactWebsite/static/js/main.480a3c4b.chunk.js"
+  },
+  {
+    "revision": "402c7cf8c6aa85ec439f",
+    "url": "/reactWebsite/static/js/runtime-main.24bb5bcb.js"
+  },
+  {
+    "revision": "f53ab0ed971322cd50d312a55458ade7",
+    "url": "/reactWebsite/static/media/profile.f53ab0ed.jpg"
+  }
+]);
